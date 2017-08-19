86a2544fffb786223d6efcce3fa4756866ed7cd9
Gopinath Jayakumar <gopinath@testleaf.com>
2017-08-19 20:14:06 +0530

    Week1Day1

---
+++ src/main/java/week1Day1/Login.java
@@ -0,0 +1,47 @@
+package week1Day1;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Login {
+
+	public static void main(String[] args) {
+		
+		//Step1:: Launch Browser
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+
+		//Step2:: Call Java Class to another Class
+		//ClassName objName = new ClassName();		
+		ChromeDriver driver = new ChromeDriver();
+		
+		//Load URL
+		driver.get("http://leaftaps.com/opentaps");
+		
+		//Maximize
+		driver.manage().window().maximize();
+		
+		//Step 4:: Enter the UserName
+		driver.findElementById("username").sendKeys("DemoSalesManager");
+		
+		//Step 5:: Enter the Password
+		driver.findElementById("password").sendKeys("crmsfa");
+		
+		//Step 6:: Click the Login Button
+		driver.findElementByClassName("decorativeSubmit").click();
+		
+		//Step 7:: Click the LogOut Button
+		driver.findElementByClassName("decorativeSubmit").click();
+		
+		//Step 8:: Close Browser.
+		driver.close();
+	}
+
+}
+
+
+
+
+
+
+
+
+
